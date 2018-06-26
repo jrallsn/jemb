b408e6a75a65326397d2cf2d07da8f57c09ff524
John Allison <john.allison@indexexchange.com>
2018-06-26 19:59:20 -0400

    ASDfasdfqw3f

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@
 const WebSocket = require('ws');
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const wsPort = 3000;
 const restPort = 4000;
@@ -15,26 +18,90 @@ const resultsDisplayTime = 10000; // time to display results in ms
 const GameStates = {
     IDLE: 'IDLE',
     COLLECTING_SUBMISSIONS: 'COLLECTING_SUBMISSIONS',
+    SHOWING_RESULT: 'SHOWING_RESULT'
 };
 
 const questions = require('./questions.json');
+
+var lastQuestion = -1;
+var lastButOneQuestion = -1;
 
 console.log(JSON.stringify(questions));
 
 var submissions = {};
 
-var gameStateObject = {
+const defaultGameState = {
     state: GameStates.IDLE,
     remainingTime: 0,
     prompt: '',
-    currentQuestionNumber: 0
+    currentQuestionNumber: 0,
+    timerStarted: 0
 };
+
+var gameStateObject = deepClone(defaultGameState);
 
 app.get('/hello', (req, res) => res.send('Hello World!'));
 
-app.get('/state', (req, res) => res.send(JSON.stringify(gameStateObject)));
+app.get('/state', (req, res) => res.json(gameStateObject));
+
+app.get('/question', (req, res) => {
+    if (gameStateObject.state !== GameStates.IDLE) {
+        res.json(gameStateObject);
+        return;
+    }
+
+    var questionNum = -1;
+
+    // Don't use either of the last two questions
+    while (questionNum === lastQuestion || questionNum === lastButOneQuestion) {
+        questionNum = Math.floor(Math.random() * questions.length);
+    }
+
+    lastButOneQuestion = lastQuestion;
+    lastQuestion = questionNum;
+
+    gameStateObject.state = GameStates.COLLECTING_SUBMISSIONS;
+    gameStateObject.remainingTime = submissionTimeLimit;
+    gameStateObject.prompt = questions[questionNum];
+    gameStateObject.currentQuestionNumber = questionNum;
+    gameStateObject.timerStarted = Date.now();
+
+    updateAllClients(gameStateObject);
+
+    res.json(gameStateObject);
+});
+
+app.post('/answer', (req, res) => {
+    var result = {
+        submittedAnswer: '',
+        numMatching: 0,
+        totalResponses: 0,
+        percent: -1
+    };
+
+    var ans = req.body.answer;
+
+    result.submittedAnswer = ans;
+    result.numMatching = submissions[gameStateObject.currentQuestionNumber][ans];
+    result.totalResponses = getTotalSubmissionsForQuestion(gameStateObject.currentQuestionNumber);
+    if (result.totalResponses > 0) {
+        result.percent = 100.0 * result.numMatching / result.totalResponses;
+    }
+
+    res.json(result);
+});
+
+app.post('/reset', (req, res) => {
+    gameStateObject = deepClone(defaultGameState);
+
+    res.json({});
+});
 
 app.listen(restPort, () => console.log('JEMB and the Holograms listening on port 4000!'));
+
+function deepClone (whatever) {
+    return JSON.parse(JSON.stringify(whatever));
+}
 
 function generateId() {
 	len = 4;
@@ -48,11 +115,21 @@ function generateId() {
     return uid;
 }
 
+function updateTimeRemaining() {
+    if (gameStateObject.state !== GameStates.COLLECTING_SUBMISSIONS) {
+        return;
+    }
+
+    gameStateObject.remainingTime = submissionTimeLimit - (Date.now() - gameStateObject.timerStarted);
+}
+
 var activeSurveySubjects = [];
 var activePlayers = [];
 var activeMasterDisplays = [];
 
 function updateOneClient (socket, updateText) {
+    updateTimeRemaining();
+
     socket.send(updateText, function(err){
         if(err){
             console.error(err);
@@ -206,7 +283,9 @@ wss.on('connection', function connection(ws) {
             case 'testMsg':
                 testMsg(data.message);
                 break;
-            
+            case 'submit':
+                submit(data.message);
+                break;
         }
 
   	});
